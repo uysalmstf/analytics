e5f362bd84644d95a6a3ce6b42c7d5a249e91354
Mustafa UYSAL <mustafa-uysal@outlook.com>
2024-07-27 17:53:15 +0300

    sql exportları

---
+++ invent_analytics_user_borrow.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
+--
+-- Host: 127.0.0.1    Database: invent_analytics
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `user_borrow`
+--
+
+DROP TABLE IF EXISTS `user_borrow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_borrow` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `status` int NOT NULL,
+  `userId` int DEFAULT NULL,
+  `bookId` int DEFAULT NULL,
+  `score` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_d11b8470b196a6fe79a68990d30` (`userId`),
+  KEY `FK_2f0369491df3141e4cb93af2135` (`bookId`),
+  CONSTRAINT `FK_2f0369491df3141e4cb93af2135` FOREIGN KEY (`bookId`) REFERENCES `book` (`id`),
+  CONSTRAINT `FK_d11b8470b196a6fe79a68990d30` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_turkish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_borrow`
+--
+
+LOCK TABLES `user_borrow` WRITE;
+/*!40000 ALTER TABLE `user_borrow` DISABLE KEYS */;
+INSERT INTO `user_borrow` VALUES (1,1,1,1,1);
+/*!40000 ALTER TABLE `user_borrow` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-27 17:52:57
